728dd8a1e5294a6cad61ef9055d7f05effd9aa0d
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-03-17 21:28:35 +0600

    Added button functionality in RegisterActivity and VerificationActivity and renamed some file maintaining standard format

--- app/src/main/java/com/example/bangla_bondhu/RegisterNumberActivity.java
+++ app/src/main/java/com/example/bangla_bondhu/RegisterNumberActivity.java
@@ -2,7 +2,9 @@ package com.example.bangla_bondhu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterNumberActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RegisterNumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_number);
+    }
+
+    public void continueClick(View view){
+        Intent intent = new Intent(this, VerificationActivity.class);
+        startActivity(intent);
     }
 }
