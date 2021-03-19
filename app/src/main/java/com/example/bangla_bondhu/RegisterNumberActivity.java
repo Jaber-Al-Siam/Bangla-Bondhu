ee4e184a00310259f1e346e3787768c9a33c5647
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-03-19 17:56:30 +0600

    Renamed file

--- app/src/main/java/com/example/bangla_bondhu/RegisterNumberActivity.java
+++ app/src/main/java/com/example/bangla_bondhu/RegisterNumberActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class RegisterNumberActivity extends AppCompatActivity {
 
@@ -15,6 +18,9 @@ public class RegisterNumberActivity extends AppCompatActivity {
     }
 
     public void continueClick(View view){
+        EditText editText = (EditText) findViewById(R.id.get_phoneNumber);
+        Editable phoneNumber = editText.getText();
+        Log.i("phoneNumber", String.valueOf(phoneNumber));
         Intent intent = new Intent(this, VerificationActivity.class);
         startActivity(intent);
     }
