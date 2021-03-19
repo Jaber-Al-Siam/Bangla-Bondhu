6ac149cd0031bf8a8c96ecb9300438347b2fb51d
safathossain <safat.hossain@northsouth.edu>
2021-03-19 14:59:58 +0600

    Signin xml working on process

---
+++ app/src/main/java/com/example/bangla_bondhu/GoogleSigninActivity.java
@@ -0,0 +1,14 @@
+package com.example.bangla_bondhu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoogleSigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.google_signin);
+    }
+}
